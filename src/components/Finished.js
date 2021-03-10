18c07a1b2a882348af105f0a5f02d2cb2e6c1d92
Lucia <lucia.okeh@gmail.com>
2021-03-09 20:52:26 -0500

    updating assets

--- src/components/Finished.js
+++ src/components/Finished.js
@@ -1,8 +1,8 @@
 import annie from "../assets/finished/annie.jpg";
 import boyyee from "../assets/finished/boyyyee.jpg";
 import dtiyss from "../assets/finished/dtiyss.jpg";
-import family from "../assets/finished/FAMILY.png";
-import friends from "../assets/finished/FRIENDS.png";
+import family from "../assets/finished/family.png";
+import friends from "../assets/finished/friends.png";
 import bro from "../assets/finished/bro.png";
 import sis from "../assets/finished/sis.png";
 import ll from "../assets/finished/ll.jpg";
@@ -12,23 +12,23 @@ import "./styles/Home.css";
 import Gallery from "./Gallery";
 
 function Finished() {
-  return (
-    <div className="Main">
-      <Gallery
-        images={[
-          annie,
-          boyyee,
-          dtiyss,
-          family,
-          friends,
-          bro,
-          sis,
-          ll,
-          snacktime,
-        ]}
-      />
-    </div>
-  );
+return (
+<div className="Main">
+    <Gallery
+    panes={[
+        {image:friends, title:"friends", tt:0, mu: "digital"},
+        {image:family, title:"friends", tt:0, mu: "digital"},
+        {image:bro, title:"friends", tt:0, mu: "digital"},
+        {image:sis, title:"friends", tt:0, mu: "digital"},
+        {image:dtiyss, title:"friends", tt:0, mu: "digital"},
+        {image:ll, title:"friends", tt:0, mu: "digital"},
+        {image:snacktime, title:"friends", tt:0, mu: "digital"},
+        {image:annie, title:"friends", tt:0, mu: "digital"},
+        {image:boyyee, title:"friends", tt:0, mu: "digital"},
+    ]}
+    />
+</div>
+);
 }
 
 export default Finished;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,33 +1,41 @@
 import "./styles/Gallery.css";
 import { useState } from "react";
 
-function Gallery({ images }) {
-  const [img, setImg] = useState(images[0]);
-  return (
-    <div className="Gallery">
-      <div className="Pane">
-        {images.map((image, index) => {
-          return (
-            <img
-              key={index}
-              src={image}
-              className="GalleryImage"
-              alt="Alt"
-              onClick={() => setImg(image)}
-            />
-          );
-        })}
-      </div>
-      <div className="Enlarged">
-        <div className="EnlargedWrapper">
-          <div
-            className="GalleryImageBig"
-            style={{ backgroundImage: `url(${img})` }}
-          />
-        </div>
-      </div>
+function Gallery({ panes }) {
+const [pane, setPane] = useState(panes[0]);
+return (
+<div className="Gallery">
+    <div className="Pane">
+    {panes.map((pane, index) => {
+        return (
+        <img
+            key={index}
+            src={pane.image}
+            className="GalleryImage"
+            alt={pane.title}
+            onClick={() => setPane(pane)}
+        />
+        );
+    })}
     </div>
-  );
+    <div className="Enlarged">
+        <div
+        className="GalleryImageBig"
+        style={{ backgroundImage: `url(${pane.image})` }}
+        />
+    </div>
+    <div className="InfoWrapper">
+        <div className="Tab">
+            INFO
+        </div>
+        <div className="Info">
+        <p>Time Taken: {pane.tt}</p>
+        <p>Media Used: {pane.mu}</p>
+        <p>Title: {pane.title}</p>
+        </div>
+    </div>
+</div>
+);
 }
 
 export default Gallery;
